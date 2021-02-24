f2bc2ba1484bdd1ac65df514b6b535b9ceed03f1
David Morrison <dm236@st-andrews.ac.uk>
2021-02-24 14:25:08 +0000

    Add Camelyon 16 dataset

---
+++ pathgen/data/datasets/camelyon16.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from typing import Dict, Tuple
+
+from sklearn.metrics import roc_curve, precision_recall_curve, auc
+import numpy as np
+import pandas as pd
+
+from repath.data.annotations import AnnotationSet
+from repath.data.annotations.asapxml import load_annotations
+from repath.data.datasets import Dataset
+from repath.data.slides.openslide import Slide
+from repath.data.slides import SlideBase
+from repath.utils.paths import project_root
+from repath.utils.metrics import conf_mat_raw, plotROC, plotROCCI, pre_re_curve
+
+
+class Camelyon16(Dataset):
+    def __init__(self, root: Path, paths: pd.DataFrame) -> None:
+        super().__init__(root, paths)
+
+    def load_annotations(self, file: Path) -> AnnotationSet:
+        # if there is no annotation file the just pass and empty list
+        annotations = load_annotations(file) if file else []
+        labels_order = ["background", "tumor", "normal"]
+        return AnnotationSet(annotations, self.labels, labels_order, "normal")
+
+    @property
+    def slide_cls(self) -> SlideBase:
+        return Slide
+
+    @property
+    def labels(self) -> Dict[str, int]:
+        return {"background": 0, "normal": 1, "tumor": 2}
+
+
+def training():
+    # set up the paths to the slides and annotations
+    root = project_root() / "data" / "camelyon16" / "raw" / "training"
+    annotations_dir = root / "lesion_annotations"
+    tumor_slide_dir = root / "tumor"
+    normal_slide_dir = root / "normal"
+
+    # all paths are relative to the dataset 'root'
+    annotation_paths = sorted(
+        [p.relative_to(root) for p in annotations_dir.glob("*.xml")]
+    )
+    tumor_slide_paths = sorted(
+        [p.relative_to(root) for p in tumor_slide_dir.glob("*.tif")]
+    )
+    normal_slide_paths = sorted(
+        [p.relative_to(root) for p in normal_slide_dir.glob("*.tif")]
+    )
+
+    # turn them into a data frame and pad with empty annotation paths
+    df = pd.DataFrame()
+    df["slide"] = tumor_slide_paths + normal_slide_paths
+    df["annotation"] = annotation_paths + ["" for _ in range(len(normal_slide_paths))]
+    df["label"] = ["tumor"] * len(tumor_slide_paths) + ["normal"] * len(
+        normal_slide_paths
+    )
+    df["tags"] = ""
+
+    return Camelyon16(root, df)
+
+
+def training_small():
+    # set up the paths to the slides and annotations
+    root = project_root() / "data" / "camelyon16" / "raw" / "training"
+    annotations_dir = root / "lesion_annotations"
+    tumor_slide_dir = root / "tumor"
+    normal_slide_dir = root / "normal"
+
+    # all paths are relative to the dataset 'root'
+    annotation_paths = sorted(
+        [p.relative_to(root) for p in annotations_dir.glob("*.xml")]
+    )
+    tumor_slide_paths = sorted(
+        [p.relative_to(root) for p in tumor_slide_dir.glob("*.tif")]
+    )
+    normal_slide_paths = sorted(
+        [p.relative_to(root) for p in normal_slide_dir.glob("*.tif")]
+    )
+
+    # turn them into a data frame and pad with empty annotation paths
+    df = pd.DataFrame()
+    df["slide"] = tumor_slide_paths + normal_slide_paths
+    df["annotation"] = annotation_paths + ["" for _ in range(len(normal_slide_paths))]
+    df["label"] = ["tumor"] * len(tumor_slide_paths) + ["normal"] * len(
+        normal_slide_paths
+    )
+    df["tags"] = ""
+
+    df = df.sample(3)
+
+    return Camelyon16(root, df)
+
+
+def testing():
+    # TODO: Add this
+    pass
---
+++ pathgen/data/datasets/dataset.py
@@ -0,0 +1,60 @@
+from abc import ABCMeta, abstractmethod
+from collections import Sequence
+from pathlib import Path
+from repath.data.slides.slide import SlideBase
+from typing import Callable, Dict, Tuple
+from repath.utils.paths import project_root
+
+import pandas as pd
+from repath.data.annotations.annotation import AnnotationSet
+
+
+class Dataset(Sequence, metaclass=ABCMeta):
+    """ A data set is an object that represents a set of slides and their annotations.
+
+    It can be used to load and iterate over a set of slides and their annotations.
+    This is an abstract base class where classes that represent specific data sets
+    should overload the load_annotations and slide_cls methods. See their descriptions
+    for details.
+    It implements the Sequence protocol so that it can be iterated over.
+
+    Args:
+        Sequence ([type]): [description]
+        metaclass ([type], optional): [description]. Defaults to ABCMeta.
+    """
+
+    def __init__(self, root: Path, paths: pd.DataFrame) -> None:
+        # process the paths_df (has two columns 'slide', 'annotation', 'label', 'tags')
+        # root is stored relative to project_root
+        self.root = root.relative_to(project_root())
+        self.paths = paths
+
+    @abstractmethod
+    def load_annotations(file: Path) -> AnnotationSet:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def slide_cls(self) -> SlideBase:
+        raise NotImplementedError
+
+    @property
+    def labels(self) -> Dict[str, int]:
+        raise NotImplementedError
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        row = self.paths.iloc[idx]
+        slide_path = self.to_abs_path(row["slide"])
+        annot_path = (
+            self.to_abs_path(row["annotation"]) if row["annotation"] != "" else ""
+        )
+        return slide_path, annot_path, row["label"], row["tags"]
+
+    def to_abs_path(self, path: Path) -> Path:
+        return project_root() / self.root / path
+
+    def to_rel_path(self, path: Path) -> Path:
+        return path.relative_to(project_root() / self.root)
